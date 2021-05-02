246d03d5c75906b6f3f2fff4d7db32c94a06ed92
richardy2971 <r1yang@ucsd.edu>
2021-05-02 02:27:08 -0700

    Completed Part 1

--- script.js
+++ script.js
@@ -2,10 +2,14 @@
 const img = new Image(); // used to load image from <input> and draw to canvas
 const canvas = document.getElementById('user-image');
 const canvas_context = canvas.getContext('2d');
-
-
 const canvas_change = document.getElementById('image-input');
-
+const generate = document.getElementById('generate-meme');
+const text_top = document.getElementById('text-top');
+const text_bottom = document.getElementById('text-bottom');
+const voice = document.getElementById('voice-selection');
+const resest = document.querySelector("[type='reset']");
+const button= document.querySelector("[type='button']");
+const volume = document.getElementById('volume-group');
 
 
 
@@ -21,18 +25,18 @@ img.addEventListener('load', () => {
 
   var every_button= document.getElementsByTagName("button");
   for(let i=0;i<every_button.length;i++){
-    if(every_button.type!="submit"){
-      every_button[i].disabled=true;
+    if(every_button[i].type =="submit"){
+      every_button[i].disabled=false;
     }
     else{
-      every_button[i].disabled=false;
+      every_button[i].disabled=true;
     }
   }
   
   canvas_context.rect(0,0,canvas.width,canvas.height);
   canvas_context.fillStyle = "black";
   canvas_context.fill();
-  
+
   var dimmensions = getDimmensions(canvas.width, canvas.height, img.width, img.height);
   canvas_context.drawImage(img, dimmensions['startX'], dimmensions['startY'], dimmensions['width'], dimmensions['height']);
 
@@ -42,6 +46,65 @@ img.addEventListener('load', () => {
 canvas_change.addEventListener('change', (event) => {
   img.src= URL.createObjectURL(event.target.files[0]);
   img.alt = event.target.files[0];
+});
+
+generate.addEventListener('submit', (event) => {
+  event.preventDefault();
+  //styling of white "meme text"
+  canvas_context.fillStyle= "white";
+  canvas_context.textAlign = "center";
+  canvas_context.font = "35px Arial";
+  canvas_context.fillText(text_top.value,canvas.width/2,30);
+  canvas_context.fillText(text_bottom.value,canvas.width/2,canvas.height-15); 
+  //turns on the voice text
+  voice.disabled=false;
+  //Selected specific type to disable or enable
+  document.querySelector("[type='reset']").disabled= false;
+  document.querySelector("[type='submit']").disabled= true;
+  document.querySelector("[type='button']").disabled= false;
+  
+});
+
+resest.addEventListener('click', (event) => {
+  event.preventDefault();
+  canvas_context.clearRect(0, 0, canvas.width, canvas.height);
+  document.querySelector("[type='reset']").disabled= true;
+  document.querySelector("[type='submit']").disabled= false;
+  document.querySelector("[type='button']").disabled= true;
+
+});
+
+
+
+button.addEventListener('click', () => {
+  var synth = window.speechSynthesis;
+  var voices = synth.getVoices();
+  var talk = new SpeechSynthesisUtterance(text_top.value+text_bottom.value);
+  talk.voice = voices[voice.value];
+  talk.volume = volume.children[1].value/100;
+  synth.speak(talk);
+});
+
+volume.addEventListener('input', () => {
+  const icon = volume.children[0];
+  const range = volume.children[1];
+  if(range.value >=67){
+    icon.src = "icons/volume-level-3.svg";
+    icon.alt = "Volume Level 3";
+
+  }
+  else if(range.value >= 33){
+    icon.src = "icons/volume-level-2.svg";
+    icon.alt = "Volume Level 2";
+  }
+  else if(range.value > 0){
+    icon.src = "icons/volume-level-1.svg";
+    icon.alt = "Volume Level 1";
+  }
+  else{
+    icon.src = "icons/volume-level-0.svg";
+    icon.alt = "Volume Level 0";
+  }
 });
 
 /**
